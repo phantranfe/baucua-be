3d0f4a4db9fbfab132e5d16b5095cc531cef203c
phantranfe <phantran.fe@gmail.com>
2026-02-26 10:11:10 +0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,59 @@
+// Dán toàn bộ ticketSets bạn cung cấp vào đây
+const ITEMS = [
+        {
+          id: "nai",
+          name: "NAI",
+          color: "171, 99, 56",
+          faceAngle: {
+            x: 0,
+            y: 0,
+          },
+        },
+        {
+          id: "ca",
+          name: "CÁ",
+          color: "47, 118, 136",
+          faceAngle: {
+            x: 0,
+            y: 90,
+          },
+        },
+        {
+          id: "bau",
+          name: "BẦU",
+          color: "126, 154, 39",
+          faceAngle: {
+            x: -90,
+            y: 0,
+          },
+        },
+        {
+          id: "cua",
+          name: "CUA",
+          color: "180, 82, 44",
+          faceAngle: {
+            x: 0,
+            y: -90,
+          },
+        },
+        {
+          id: "ga",
+          name: "GÀ",
+          color: "175, 43, 45",
+          faceAngle: {
+            x: 90,
+            y: 0,
+          },
+        },
+        {
+          id: "tom",
+          name: "TÔM",
+          color: "192, 55, 83",
+          faceAngle: {
+            x: 0,
+            y: 180,
+          },
+        },
+      ];
+
+module.exports = { ITEMS };
